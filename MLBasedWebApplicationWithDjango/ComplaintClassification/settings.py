a08ca11785e0a13386d5a24d61916d4d3031d420
Mehmet Emin Aydın <meaydin.ceng@gmail.com>
2022-03-30 13:03:22 +0300

    Update settings.py

--- MLBasedWebApplicationWithDjango/ComplaintClassification/settings.py
+++ MLBasedWebApplicationWithDjango/ComplaintClassification/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER' : 'postgres',
-        'PASSWORD' : '753951456',
+        'PASSWORD' : 'password',
         'HOST' : 'localhost',
         'PORT': '5432',
     }
